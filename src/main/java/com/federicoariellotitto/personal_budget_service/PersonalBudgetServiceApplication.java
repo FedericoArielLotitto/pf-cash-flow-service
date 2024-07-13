40aebc0abc01109ad5a7b812b2fdf4319e6a9a16
FedericoArielLotitto <federicoariellotitto@gmail.com>
2024-07-13 00:54:52 -0300

    Initial commit

---
+++ src/main/java/com/federicoariellotitto/personal_budget_service/PersonalBudgetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.federicoariellotitto.personal_budget_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBudgetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBudgetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/federicoariellotitto/personal_budget_service/PersonalBudgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.federicoariellotitto.personal_budget_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBudgetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
