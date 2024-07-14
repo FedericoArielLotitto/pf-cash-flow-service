03221699d777ecdc6af267acefae51408c91d1ac
FedericoArielLotitto <federicoariellotitto@gmail.com>
2024-07-14 19:49:17 -0300

    refactor: renames json properties of error responses

--- src/main/java/com/federicoariellotitto/personal_budget_service/controller/dto/ErrorResponseDTO.java
+++ src/main/java/com/federicoariellotitto/personal_budget_service/controller/dto/ErrorResponseDTO.java
@@ -1,13 +1,18 @@
 package com.federicoariellotitto.personal_budget_service.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 public record ErrorResponseDTO(
+        @JsonProperty("api_path")
         String apiPath,
+        @JsonProperty("error_code")
         HttpStatus errorCode,
+        @JsonProperty("error_message")
         String errorMessage,
+        @JsonProperty("error_time")
         LocalDateTime errorTime
 ) {
 }
